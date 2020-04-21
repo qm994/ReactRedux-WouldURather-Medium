23862d00675e9d13d8c4cb542b80ca0471de8cf1
Qingyuan Ma <qm28@georgetown.edu>
2020-04-21 16:48:47 -0400

    reducer,action for authedUser

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import polls from './polls';
 import users from './users';
+import authedUser from './authedUser';
 
 import { combineReducers } from 'redux';
 
 export default combineReducers({
     users: users,
-    polls: polls
+    polls: polls,
+    authedUser: authedUser
 })
