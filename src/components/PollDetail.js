08f957c550d6daf8b742963844069e3ec4a88ca4
Qingyuan Ma <qm28@georgetown.edu>
2020-04-27 21:35:43 -0400

    set the style for pollDetail

--- src/components/PollDetail.js
+++ src/components/PollDetail.js
@@ -1,20 +1,85 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import ReactDOM from 'react-dom';
+import Image from 'react-bootstrap/Image';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
+import ListGroup from 'react-bootstrap/ListGroup';
+import Form from 'react-bootstrap/Form';
 
 class PollDetail extends Component {
+    state = {
+        checked: false,
+        option: ""
+    }
+
+    onClick = (event, val) => {
+        // this.setState({
+        //     checked: event.target.checked,
+        //     option: val
+        // })
+    }
+
+    onSubmit = (event, checkId) => {
+        event.preventDefault();
+        //const selectedVal = document.querySelector(".checkId:checked").value
+        
+        console.log(event.target)
+    }
     render() {
+        const { id, polls, users, authedUser } = this.props;
+        const pollInfo = polls[id];
+        const authorId = pollInfo.author;
+        const authorInfo = users[authorId];
+        console.log(authorInfo, polls[id])
         return (
-        <div><h3>Poll Detail: {this.props.id}</h3></div>
+            <Form onSubmit={(event)=> this.onSubmit(event, pollInfo.id)}>
+                <Image roundedCircle fluid src={authorInfo.avatarURL} className="avatar" />
+                <Card.Body>
+                    <Card.Title>{authorInfo.name} asks:</Card.Title>
+                    <Card.Subtitle>
+                        Would U Rather...
+                    </Card.Subtitle>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                    <ListGroup.Item>
+                        <Form.Check
+                            type="checkbox"
+                            className={pollInfo.id}
+                            label={pollInfo.optionOne.text}
+                            value="optionOne"
+                            checked={this.state.checked}
+                            onClick={this.onClick(id)}
+                        />
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <Form.Check
+                            type="checkbox"
+                            className={pollInfo.id}
+                            label={pollInfo.optionTwo.text}
+                            value="optionTwo"
+                            checked={this.state.checked}
+                            onClick={this.onClick(id)}
+                        />
+                    </ListGroup.Item>
+                </ListGroup>
+                <br />
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
         )
     }
 };
 
-function mapStateToProps({ }, props) {
+function mapStateToProps({ polls, users, authedUser }, props) {
     const id = props.match.params.id
-    console.log(props.match.params.id)
     return {
-        id
+        id,
+        polls,
+        users,
+        authedUser
     }
 }
 export default connect(mapStateToProps)(PollDetail);
