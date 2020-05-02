01903d2a5ce831f00db378eccb20a411dfdd46bb
Qingyuan Ma <qm28@georgetown.edu>
2020-05-01 23:08:41 -0400

    fulfill the leaderboard component

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,27 +1,51 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
+import Figure from 'react-bootstrap/Figure';
+import FigureImage from 'react-bootstrap/FigureImage';
 
-
+import "../css/index.css";
 class LeaderBoard extends Component {
-    
+
     render() {
-        console.log(this.props.leaderData)
         return (
             <div>
-                
-                <Card>
+                {this.props.leaderData.map(data => (
+                    <Card key={data.id} style={{ width: '50rem' }}>
+                        <Card.Body>
+                            <Card.Title>{data.name}</Card.Title>
 
-                </Card>
+                            <FigureImage
+                                width={171}
+                                height={180}
+                                src={data.avatarURL}
+                                alt={data.name}
+                                fluid
+                                roundedCircle
+                            />
+                            <div className="leader">
+                                <Card.Text as="h5">
+                                    Answerd Questions: <span>{data.answered}</span>
+                                </Card.Text>
+                                <Card.Text as="h5">
+                                    Created Questions: <span>{data.created}</span>
+                                </Card.Text>
+                            </div>
+                        </Card.Body>
+                        
+                    </Card>
+                ))}
             </div>
         )
     }
 };
 
-function mapStateToProps({users, authedUser}) {
+function mapStateToProps({ users, authedUser }) {
     const leaderData = Object.keys(users).map(id => ({
         id: id,
         name: users[id].name,
+        avatarURL: users[id].avatarURL,
         created: users[id].questions.length,
         answered: Object.keys(users[id].answers).length
     }));
